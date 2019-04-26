5cc1a043d6af14dd78560926bd48a5dedf1f40e0
Anny Guttierrez <agutilop@everis.com>
2019-04-25 23:25:48 -0500

    Making a post selectable
    - Making a post selectable.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,27 +8,37 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-          .then(response => {
-              const posts = response.data.slice(0,4);
-              const updatedPosts = posts.map(post => {
-                  return {
-                      ...post,
-                      author: 'Max'
-                  }
-              })
-              this.setState({ posts: updatedPosts });
-              // console.log(response);
-          })
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+                // console.log(response);
+            })
     }
 
-    render () {
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id })
+    }
+
+    render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
         })
         return (
             <div>
@@ -36,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
