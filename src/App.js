603b151b0d079de9f4d492d45e021aa445abbfbc
Anny Guttierrez <agutilop@everis.com>
2019-04-28 22:59:13 -0500

    Setting up the router package
    - Setting up the router package.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-
+// Technically only react-router-dom is required for web development.
+// It wraps react-router  and therefore uses it as a dependency. 
+// We don't need to install react-router  on our own for it to work. You can omit this installation step.
+import { BrowserRouter } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
