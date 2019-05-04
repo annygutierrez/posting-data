629e3f91ed6068553d55b302f75dd65a7b129d7e
Anny Fiorella Gutierrez Lopez <annygutierrez@MacBook-Pro-de-Anny.local>
2019-05-04 12:13:58 -0500

    Pass new parameter id
    - Pass new parameter id.

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,57 +1,61 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
+import { Link } from 'react-router-dom';
 import './Posts.css';
 
 class Posts extends Component {
-    state = {
-        posts: []
-    } 
+  state = {
+    posts: []
+  }
 
-    componentDidMount() {
-        // This is pass by react-router
-        console.log(this.props);
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                })
-                this.setState({ posts: updatedPosts });
-                // console.log(response);
-            })
-            .catch(error => {
-                console.log(error)
-                // this.setState({ error: true })
-            })
-    }
+  componentDidMount() {
+    // This is pass by react-router
+    console.log(this.props);
+    axios.get('/posts')
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        })
+        this.setState({ posts: updatedPosts });
+        // console.log(response);
+      })
+      .catch(error => {
+        console.log(error)
+        // this.setState({ error: true })
+      })
+  }
 
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id })
-    }
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id })
+  }
 
-    render() {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    // match={this.props.match}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            })
-        }
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+          <Link key={post.id}  to={'/' + post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              // match={this.props.match}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+          </Link>
         )
+      })
     }
+    return (
+      <section className="Posts">
+        {posts}
+      </section>
+    )
+  }
 }
 
 export default Posts;
